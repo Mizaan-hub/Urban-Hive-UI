4317c3b40bce801bf3ed453e82fe9df20b66edae
Mizaan-hub <mizaanafandi@gmail.com>
2024-05-25 03:10:15 +0530

    Added Home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import NavBar from './components/NavBar/NavBar'
 import './layout.scss'
+import HomePage from './routes/HomePage/HomePage'
 
 function App() {
   return (
     <div className="layout">
-      <NavBar />
+      
+      <div className="NavBar">
+        <NavBar />
+      </div>
+      <div className="content">
+        <HomePage />
+      </div>
+
     </div>
   )
 }
---
+++ src/routes/HomePage/HomePage.jsx
@@ -0,0 +1,22 @@
+import './HomePage.scss'
+
+function HomePage (){
+    return(
+        <div className='HomePage'>
+
+            <div className="textContainer">
+
+                <div className="wrapper">
+                <h1 className='title'>Find Real Estate & Get Your Dream Place</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel possimus deserunt autem quaerat sint qui aperiam mollitia ea rem sunt.</p>
+                </div>
+
+            </div>
+            <div className="imgContainer">
+                <img src="/public/assets/images/bg.png" alt="" />
+            </div>
+
+        </div>
+    )
+}
+export default HomePage
