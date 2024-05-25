a4d44aab157370062e1a803bbfee2d0616fb00ff
Mizaan-hub <mizaanafandi@gmail.com>
2024-05-26 01:48:23 +0530

    Created Responsive SearchBar for HomePage

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,8 +1,51 @@
+import { useState } from 'react'
 import './SearchBar.scss'
 
+const types = ["buy", "rent"];
+
 function SearchBar (){
+
+    const [query,setQuery] = useState({
+        type:"buy",
+        location:"",
+        minPrice:"0",
+        maxPrice:"0",
+    })
+
+    const switchType = (val) =>{
+        setQuery((prev) => ({...prev,type: val}));
+    }
+
     return(
-        <div className='SearchBar'>SearchBar</div>
+        <div className='SearchBar'>
+            <div className="type"> 
+                {types.map((type) => (
+                    <button key={type} onClick={() => switchType(type)} className={query.type === type ? "active" : ""}>
+                        {type}
+                    </button>
+                ))}
+            </div>
+            <form action="">
+                <input type="text" name="location" placeholder='City Location' />
+                <input 
+                type="number" 
+                name="minPrice" 
+                placeholder='Minimum Price' 
+                min={0} 
+                max={10000000}
+                />
+                <input 
+                type="number" 
+                name="maxPrice" 
+                placeholder='Maximum Price'
+                min={0}
+                max={10000000}
+                />
+                <button>
+                    <img src="/public/assets/images/search.png" alt="" />
+                </button>
+            </form>
+        </div>
     )
 }
 export default SearchBar
