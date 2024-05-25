c7a6be56e8bcee3e6ffb90c4dcf7669fc8361377
Mizaan-hub <mizaanafandi@gmail.com>
2024-05-25 12:14:20 +0530

    Updated Home page

---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,8 @@
+import './SearchBar.scss'
+
+function SearchBar (){
+    return(
+        <div className='SearchBar'>SearchBar</div>
+    )
+}
+export default SearchBar
--- src/routes/HomePage/HomePage.jsx
+++ src/routes/HomePage/HomePage.jsx
@@ -1,3 +1,4 @@
+import SearchBar from '../../components/SearchBar/SearchBar'
 import './HomePage.scss'
 
 function HomePage (){
@@ -5,15 +6,32 @@ function HomePage (){
         <div className='HomePage'>
 
             <div className="textContainer">
-
                 <div className="wrapper">
-                <h1 className='title'>Find Real Estate & Get Your Dream Place</h1>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel possimus deserunt autem quaerat sint qui aperiam mollitia ea rem sunt.</p>
-                </div>
 
+                    <h1 className='title'>Find Real Estate & Get Your Dream Place</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel possimus deserunt autem quaerat sint qui aperiam mollitia ea rem sunt.</p>
+                    <SearchBar/>
+                    <div className="boxes">
+                        <div className="box">
+                            <h1>69+</h1>
+                            <h2>Years Of Experience</h2>
+                        </div>
+                        <div className="box">
+                            <h1>690</h1>
+                            <h2>Awards Gained</h2>
+                        </div>
+                        <div className="box">
+                            <h1>6969+</h1>
+                            <h2>Property Ready</h2>
+                        </div>
+                    </div>
+
+                </div>
             </div>
+
             <div className="imgContainer">
-                <img src="/public/assets/images/bg.png" alt="" />
+                {/* <img src="/public/assets/images/bg.png" alt="" /> */}
+                <img src="/public/assets/pics/Urban Hive.gif" alt="" />
             </div>
 
         </div>
