0393a76a6cb4e962edc005c503f2e429df2ca68a
Mizaan-hub <mizaanafandi@gmail.com>
2024-05-28 02:11:58 +0530

    Comppleted Whole SinglePage with responsive media queries

--- src/lib/dummydata.js
+++ src/lib/dummydata.js
@@ -115,6 +115,6 @@ export const listData = [
   
   export const userData = {
     id: 1,
-    name: "John Doe",
-    img: "https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    name: "Lana Rhoades",
+    img: "/public/assets/pics/lana-profile-2.jpg",
   };
