8dc8de0049ce417cee94c6d8211ea52d0c513036
Mizaan-hub <mizaanafandi@gmail.com>
2024-06-07 01:49:33 +0530

    Full working search featu

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,51 +1,110 @@
-import './Filter.scss'
+import { useState } from "react";
+import "./Filter.scss";
+import { useSearchParams } from "react-router-dom";
 
-function Filter (){
-    return(
-        <div className='Filter'>
-            <h1>Search Results For <b>London</b> </h1>
-            <div className="top">
-                <div className="item">
-                    <label htmlFor="city">Location</label>
-                    <input type="text" name="city" id="city" placeholder='City location' />
-                </div>
-            </div>
-            <div className="bottom">
-                <div className="item">
-                    <label htmlFor="type">Type</label>
-                    <select name="type" id="type">
-                        <option value="">any</option>
-                        <option value="buy">Buy</option>
-                        <option value="rent">Rent</option>
-                    </select>
-                </div>
-                <div className="item">
-                    <label htmlFor="property">Property</label>
-                    <select name="property" id="property">
-                        <option value="">any</option>
-                        <option value="apartment">Apartment</option>
-                        <option value="house">House</option>
-                        <option value="condo">Condo</option>
-                        <option value="land">Land</option>
-                    </select>
-                </div>
-                <div className="item">
-                    <label htmlFor="minPrice">Min Price</label>
-                    <input type="number" name="minPrice" id="minPrice" placeholder='any' min={0} max={10000000}/>
-                </div>
-                <div className="item">
-                    <label htmlFor="maxPrice">Max Price</label>
-                    <input type="number" name="maxPrice" id="maxPrice" placeholder='any' min={0} max={100000}/>
-                </div>
-                <div className="item">
-                    <label htmlFor="bedroom">Bedroom</label>
-                    <input type="text" name="bedroom" id="bedroom" placeholder='any' />
-                </div>
-                <button>
-                    <img src="/public/assets/images/search.png" alt="" />
-                </button>
-            </div>
+function Filter() {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const [query, setQuery] = useState({
+    type: searchParams.get("type") || "",
+    city: searchParams.get("city") || "",
+    property: searchParams.get("property") || "",
+    minPrice: searchParams.get("minPrice") || 0,
+    maxPrice: searchParams.get("maxPrice") || 10000000,
+    bedroom: searchParams.get("bedroom") || 1,
+  });
+
+  const handleChange = (e) => {
+    setQuery({
+      ...query,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleFilter = () => {
+    setSearchParams(query);
+  }
+
+
+  return (
+    <div className="Filter">
+      <h1>
+        Search Results For <b>{searchParams.get("city") }</b>{" "}
+      </h1>
+      <div className="top">
+        <div className="item">
+          <label htmlFor="city">Location</label>
+          <input
+            type="text"
+            name="city"
+            id="city"
+            placeholder="City location"
+            onChange={handleChange}
+            defaultValue={query.city}
+          />
         </div>
-    )
+      </div>
+      <div className="bottom">
+        <div className="item">
+          <label htmlFor="type">Type</label>
+          <select name="type" id="type" onChange={handleChange} defaultValue={query.type}>
+            <option value="">any</option>
+            <option value="buy">Buy</option>
+            <option value="rent">Rent</option>
+          </select>
+        </div>
+        <div className="item">
+          <label htmlFor="property">Property</label>
+          <select name="property" id="property" onChange={handleChange} defaultValue={query.property}>
+            <option value="">any</option>
+            <option value="apartment">Apartment</option>
+            <option value="house">House</option>
+            <option value="condo">Condo</option>
+            <option value="land">Land</option>
+          </select>
+        </div>
+        <div className="item">
+          <label htmlFor="minPrice">Min Price</label>
+          <input
+            type="number"
+            name="minPrice"
+            id="minPrice"
+            placeholder="any"
+            min={0}
+            max={10000000}
+            onChange={handleChange}
+            defaultValue={query.minPrice}
+          />
+        </div>
+        <div className="item">
+          <label htmlFor="maxPrice">Max Price</label>
+          <input
+            type="number"
+            name="maxPrice"
+            id="maxPrice"
+            placeholder="any"
+            min={0}
+            max={100000}
+            onChange={handleChange}
+            defaultValue={query.maxPrice}
+          />
+        </div>
+        <div className="item">
+          <label htmlFor="bedroom">Bedroom</label>
+          <input
+            type="text"
+            name="bedroom"
+            id="bedroom"
+            placeholder="any"
+            onChange={handleChange}
+            defaultValue={query.bedroom}
+          />
+        </div>
+        <button onClick={handleFilter}>
+          <img src="/public/assets/images/search.png" alt="" />
+        </button>
+      </div>
+    </div>
+  );
 }
-export default Filter
+export default Filter;
